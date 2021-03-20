6c99231add7b75b0763df1f3021c7c0343809001
Mayank Dusk <mayankdusk@gmail.com>
2021-03-20 16:36:02 +0530

    arthematic

--- test.js
+++ test.js
@@ -1,1 +1,133 @@
-console.log("hello World")
+var x = ("150 + 50")
+ console.log(x);
+
+a =2
+c =5
+var x = a+c
+console.log (x);
+
+a =6
+var x =(100+50)*a;
+console.log(x);
+
+
+var x =4
+var y =5
+var z =x+y
+console.log(x+y);
+
+var t =6
+var s =7
+var w=t/s
+console.log(t/s)
+
+var c =3
+var f =12
+console.log(f%c)
+
+
+var s = 4
+var post= s++
+
+console.log(s++);
+
+var pre = ++s
+console.log(post)
+console.log(pre)
+
+var w =3
+var post =w--
+console.log(w--);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+a =101
+b = 90
+c = 120
+
+d = 90
+e = 60
+
+// if (a > 60){
+//     console.log("a is passed");
+// }
+
+// console.log("a is failed");
+
+// if ( a > 60){
+//     console.log("a is passed");
+
+// }else{
+//     console.log("a is failed");
+
+// }
+
+// if (a > b){
+//     if(a > c){
+//         console.log("a is greatest one");
+//     }
+//     else{
+//         if (c > b){
+//             console.log("C is greatest one ");
+//         }
+//     }
+// }else{
+//     if (b>c){
+//         console.log("B is greatest one");
+//     }
+// }
+
+// if(condtion){
+
+// }
+// else if(condtion){
+
+// }
+// else if(condition){
+
+// }else{
+
+// }
